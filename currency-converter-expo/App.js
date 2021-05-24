2b76fc8d3861537571a68e318c4ad71a19023dae
Amitojasa <singh99amitoj@gmail.com>
2021-05-25 01:26:35 +0530

    Adding currency values, Formating text input & result container

--- currency-converter-expo/App.js
+++ currency-converter-expo/App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	SafeAreaView,
+	ScrollView,
+	TextInput,
+	Text,
+	View,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const currencyPerRupee = {
+	DOLLAR: 0.014,
+	EURO: 0.012,
+	POUND: 0.011,
+	RUBEL: 0.93,
+	AUSDOLLAR: 0.2,
+	CANDOLLAR: 0.019,
+	YEN: 1.54,
+	DINAR: 0.0043,
+	BITCOIN: 0.000004,
+};
+
+const App = () => {
+	const [inputValue, setInputValue] = useState(0);
+	const [resultValue, setResultValue] = useState(0);
+
+	return (
+		<>
+			<ScrollView backgroundColor="#1b262c">
+				<SafeAreaView style={styles.container}>
+					<View style={styles.resultContainer}>
+						<Text style={styles.resultValue}>22.3</Text>
+					</View>
+					<View style={styles.inputContainer}>
+						<TextInput
+							style={styles.input}
+							keyboardType="numeric"
+							placeholder="Enter Value"
+							placeholderTextColor="#FFFFFF"
+						/>
+					</View>
+				</SafeAreaView>
+			</ScrollView>
+		</>
+	);
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#1b262c",
+	},
+	resultContainer: {
+		height: 70,
+		marginTop: 80,
+		justifyContent: "center",
+		borderColor: "#bbe1fa",
+		borderWidth: 2,
+		alignItems: "center",
+	},
+
+	resultValue: {
+		fontSize: 30,
+		color: "#ffffff",
+		fontWeight: "bold",
+	},
+	inputContainer: {
+		height: 70,
+		marginTop: 10,
+		justifyContent: "center",
+		alignItems: "center",
+		borderWidth: 2,
+		borderColor: "#bbe1fa",
+	},
+	input: {
+		fontSize: 20,
+		textAlign: "center",
+		color: "#FFFFFF",
+	},
 });

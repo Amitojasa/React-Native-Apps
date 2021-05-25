75b777ea0f16f0f0e9f2a320dd76b136d5f22bc8
Amitojasa <singh99amitoj@gmail.com>
2021-05-25 18:25:59 +0530

    Buutons,keyboard dismiss and functioning

--- currency-converter-expo/App.js
+++ currency-converter-expo/App.js
@@ -6,13 +6,16 @@ import {
 	TextInput,
 	Text,
 	View,
+	TouchableOpacity,
+	Alert,
+	StatusBar,
 } from "react-native";
 
 const currencyPerRupee = {
 	DOLLAR: 0.014,
 	EURO: 0.012,
 	POUND: 0.011,
-	RUBEL: 0.93,
+	RUBLE: 0.93,
 	AUSDOLLAR: 0.2,
 	CANDOLLAR: 0.019,
 	YEN: 1.54,
@@ -21,15 +24,31 @@ const currencyPerRupee = {
 };
 
 const App = () => {
-	const [inputValue, setInputValue] = useState(0);
+	const [inputValue, setInputValue] = useState("");
 	const [resultValue, setResultValue] = useState(0);
+
+	const convertCurrency = (currency) => {
+		if (!inputValue) {
+			return Alert.alert("Error", "Please enter some value");
+		}
+		let r = parseFloat(inputValue) * currencyPerRupee[currency];
+		setResultValue(r.toFixed(3));
+	};
 
 	return (
 		<>
-			<ScrollView backgroundColor="#1b262c">
+			<StatusBar />
+			<ScrollView
+				backgroundColor="#1b262c"
+				keyboardShouldPersistTaps="never"
+				contentInsetAdjustmentBehavior="automatic"
+			>
 				<SafeAreaView style={styles.container}>
+					<View>
+						<Text style={styles.title}>Currency Converter</Text>
+					</View>
 					<View style={styles.resultContainer}>
-						<Text style={styles.resultValue}>22.3</Text>
+						<Text style={styles.resultValue}>{resultValue}</Text>
 					</View>
 					<View style={styles.inputContainer}>
 						<TextInput
@@ -37,7 +56,24 @@ const App = () => {
 							keyboardType="numeric"
 							placeholder="Enter Value"
 							placeholderTextColor="#FFFFFF"
+							value={inputValue.toString()}
+							onChangeText={(inputValue) =>
+								setInputValue(inputValue)
+							}
 						/>
+					</View>
+					<View style={styles.convertButtonContainer}>
+						{Object.keys(currencyPerRupee).map((currency) => (
+							<TouchableOpacity
+								key={currency}
+								style={styles.currencyBox}
+								onPress={() => convertCurrency(currency)}
+							>
+								<Text style={styles.currencyText}>
+									{currency}
+								</Text>
+							</TouchableOpacity>
+						))}
 					</View>
 				</SafeAreaView>
 			</ScrollView>
@@ -52,9 +88,15 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: "#1b262c",
 	},
+	title: {
+		fontSize: 40,
+		marginTop: 30,
+		color: "#FFF",
+		alignSelf: "center",
+	},
 	resultContainer: {
 		height: 70,
-		marginTop: 80,
+		marginTop: 30,
 		justifyContent: "center",
 		borderColor: "#bbe1fa",
 		borderWidth: 2,
@@ -78,5 +120,26 @@ const styles = StyleSheet.create({
 		fontSize: 20,
 		textAlign: "center",
 		color: "#FFFFFF",
+	},
+
+	convertButtonContainer: {
+		flexDirection: "row",
+		flexWrap: "wrap",
+		marginTop: 10,
+	},
+	currencyBox: {
+		height: 100,
+		width: "33.3%",
+		justifyContent: "center",
+		alignItems: "center",
+		borderWidth: 2,
+		borderColor: "#bbe1fa",
+		backgroundColor: "#0f4c75",
+		marginVertical: 10,
+	},
+	currencyText: {
+		color: "#FFFFFF",
+		// fontSize: 20,
+		fontWeight: "bold",
 	},
 });

f2e11e4b06cfd3b6330d6a08e7e1d0fdc0e0f986
Amitojasa <singh99amitoj@gmail.com>
2021-06-10 01:02:47 +0530

    working on redux app

---
+++ reduxappnpx/src/action/list.js
@@ -0,0 +1,14 @@
+import { ADD_SERIES, MARK_COMPLETE, REMOVE_SERIES } from "./action.types"
+
+export const addSeries = (series) => ({
+    type: ADD_SERIES,
+    payload: series
+})
+export const removeSeries = (id) => ({
+    type: REMOVE_SERIES,
+    payload: series
+})
+export const markComplete = (id) => ({
+    type: MARK_COMPLETE,
+    payload: id
+})
